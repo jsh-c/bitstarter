f3bb90edb43e0d964dd062e0d1efedcc22fa75ad
Ubuntu <ubuntu@ip-10-154-159-203.ec2.internal>
2013-07-10 20:31:01 +0000

    wip

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('./restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,12 +62,32 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var saveToFile = function(tmpFile) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            console.error("Wrote %s", tmpfile);
+            fs.writeFileSync(tmpfile, result);
+        }
+    };
+    return response2console;
+};
+
+var loadUrl  = function(url, file) {
+    
+    rest.get(url).on('complete', saveToFile(file);
+    return file;
+}
+
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var inputFile = program.url ? loadUrl(program.url) : program.file;
+    var checkJson = checkHtmlFile(inputFile, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
